8febd888b859e99fc41b4df2cbaf512dd24500e2
thbdias <thiagobalbino91@gmail.com>
2019-02-07 13:03:06 -0200

    rescue from 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
+
+  # vai recuperar o app quando acontecer alguma coisa em qualquer controller
+  # como -> not_found, erro interno...
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+
+  def render_404
+    # mandando para a home
+    redirect_to main_app.root_url
+  end
 end
